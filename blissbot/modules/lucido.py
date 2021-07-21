d0f05bf309492634e2a7212376765feb5784423f
Lᴜᴄɪᴅᴏ™ <74089102+LucidoXD@users.noreply.github.com>
2021-07-21 17:06:46 +0530

    Update lucido.py

--- blissbot/modules/lucido.py
+++ blissbot/modules/lucido.py
@@ -21,12 +21,12 @@ def yt_search(song):
         return url
 
 
-@app.on_message(filters.create(ignore_blacklisted_users) & filters.command("song"))
+@app.on_message(filters.create(ignore_blacklisted_users) & filters.command("bsong"))
 async def song(client, message):
     chat_id = message.chat.id
     user_id = message.from_user["id"]
     add_chat_to_db(str(chat_id))
-    args = get_arg(message) + " " + "song"
+    args = get_arg(message) + " " + "bsong"
     if args.startswith(" "):
         await message.reply("Enter a song name. Check /help")
         return ""

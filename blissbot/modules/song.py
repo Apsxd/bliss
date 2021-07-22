139863a416d7a6cf81616bc055151726bea12cd0
Lᴜᴄɪᴅᴏ™ <74089102+LucidoXD@users.noreply.github.com>
2021-07-22 19:27:41 +0530

    Create song.py

---
+++ blissbot/modules/song.py
@@ -0,0 +1,8 @@
+__mod_name__ = "◎Music"
+
+__help__ = """
+• `/song`** <songname artist(optional)>: download the song in it's best quality available.(API BASED)
+• `/video`** <songname artist(optional)>: download the video song in it's best quality available.
+• `/lyrics`** <songname artist(optional)>: sends the complete lyrics of the song provided as input
+• `/glyrics`** <i> song name </i> : This plugin searches for song lyrics with song name and artist.
+"""

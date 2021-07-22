7a5a2ebbf8c5755b5d7dfe08022cbbcf3f24f23e
Lᴜᴄɪᴅᴏ™ <74089102+LucidoXD@users.noreply.github.com>
2021-07-22 17:37:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 DATABASE_URL = os.getenv("DATABASE_URL")
 OWNER_ID = list({int(x) for x in os.environ.get("OWNER_ID", "").split()})
+# lucidoo
